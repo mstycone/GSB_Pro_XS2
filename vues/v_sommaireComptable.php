5135d639ed50f3caf87ea6a4552424b4fac170fc
mstycone <msmagicone@gmail.com>
2024-06-17 10:26:40 +0200

    MàJ

--- vues/v_sommaireComptable.php
+++ vues/v_sommaireComptable.php
@@ -1,4 +1,5 @@
 ﻿    <!-- Division pour le sommaire -->
+  <!-- À modifier selon les caractéristiques du comptable-->
     <div id="menuGauche">
      <div id="infosUtil">
     
@@ -7,17 +8,17 @@
       </div>  
         <ul id="menuList">
 			<li >
-				  comptable :<br>
-            <!-- Affichage des informations sur le comptable -->
+				  visiteur :<br>
+            <!-- Affichage des informations sur le visiteur -->
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
 			</li>
            <li class="smenu">
-            <!-- Lien pour valider la fiche de frais -->
-              <a href="index.php?uc=gererFrais&action=validerCreationFrais" title="Valider fiche de frais ">Valider fiche de frais</a>
+            <!-- Lien pour la saisie de la fiche de frais -->
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
            </li>
            <li class="smenu">
-            <!-- Lien pour le suivie du paiement des fiches de frais -->
-              <a href="index.php?uc=gererFrais&action=validerMajFraisForfait" title="Suivre paiement fiches de frais">Suivre paiement fiches de frais</a>
+            <!-- Lien pour la consultation des fiches de frais -->
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
            </li>
  	   <li class="smenu">
             <!-- Lien pour la déconnexion -->
